ef47227384bceece17a715357362e76633778618
dharman01kothari <65413671+dharman01kothari@users.noreply.github.com>
2020-06-11 17:58:59 -0400

    up rev
    some code

--- robo_advisor.py
+++ robo_advisor.py
@@ -1,21 +1,26 @@
 # app/robo_advisor.py
 import json
 import requests
+import datetime
 
 
-print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
-print("-------------------------")
-print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
-print("-------------------------")
-print("LATEST DAY: 2018-02-20")
-print("LATEST CLOSE: $100,000.00")
-print("RECENT HIGH: $101,000.00")
-print("RECENT LOW: $99,000.00")
-print("-------------------------")
-print("RECOMMENDATION: BUY!")
-print("RECOMMENDATION REASON: TODO")
-print("-------------------------")
-print("HAPPY INVESTING!")
-print("-------------------------")
+if __name__ == "__main__":
+
+    stock = input('Enter your stock symbol:')
+
+    print("-------------------------")
+    print("SELECTED SYMBOL:", stock)
+    print("-------------------------")
+    print("REQUESTING STOCK MARKET DATA...")
+    print("REQUEST AT:",datetime.datetime.now())
+    print("-------------------------")
+    print("LATEST DAY: 2018-02-20")
+    print("LATEST CLOSE: $100,000.00")
+    print("RECENT HIGH: $101,000.00")
+    print("RECENT LOW: $99,000.00")
+    print("-------------------------")
+    print("RECOMMENDATION: BUY!")
+    print("RECOMMENDATION REASON: TODO")
+    print("-------------------------")
+    print("HAPPY INVESTING!")
+    print("-------------------------")
